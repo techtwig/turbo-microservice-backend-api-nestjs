fb0144ebef1a59008654e9aac293de74087761e5
hsnbd <hasanbd666@gmail.com>
2023-02-18 20:36:26 +0600

    fix import issue

--- apps/turbo/src/post-module/post.module.ts
+++ apps/turbo/src/post-module/post.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { PostCategoryModule } from './post-categories/post-category.module';
-import { PostController } from './Post.controller';
-import { PostService } from './Post.service';
+import { PostController } from './post.controller';
+import { PostService } from './post.service';
 import { Post, PostSchema } from './schema/post.schema';
 
 @Module({
